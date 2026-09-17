9b0c8e9b4cc328c0ba29636daad3da65a4e482b8
Anna <annacarolsechin@gmail.com>
2026-09-17 08:20:16 -0300

    feat: adiciona aplicação desktop para gerenciamento de pets

---
+++ PetsCity/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PetsCity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Negocio negocio = new Negocio();
+
+                negocio.NomePet = txtNomePet.Text;
+                negocio.PrecoServ = Convert.ToDouble(txtPrecoServico.Text);
+                negocio.Quantidade = int.Parse(txtQuantidade.Text);
+                negocio.IdadeAnimal = int.Parse(txtIdadeAnimal.Text);
+
+                double desconto = negocio.CalcularDesconto();
+                double valorFinal = negocio.CalcularValorFinal();
+                double parcela = negocio.CalcularParcela();
+                bool brinde = negocio.ValidarBrinde();
+
+                lblNome.Text = $"{negocio.NomePet.ToUpper()}";
+                lblDesconto.Text = $"{desconto:N2}";
+                lblValorFinal.Text = $"{valorFinal:N2}";
+                lblParcela.Text = $"3x de R${parcela:N2}";
+
+                txtNomePet.Clear();
+                txtPrecoServico.Clear();
+                txtQuantidade.Clear();
+                txtIdadeAnimal .Clear();
+
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Preencha os campos corretamente",
+                                "Erro",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error
+                                );
+            }
+        }
+    }
+}
---
+++ PetsCity/Negocio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsCity
+{
+    internal class Negocio
+    {
+        public string NomePet {  get; set; }
+        public double PrecoServ { get; set; }
+        public int Quantidade {  get; set; }
+        public int IdadeAnimal {  get; set; }
+
+        public double CalcularDesconto()
+        {
+            double valorTotal = PrecoServ * Quantidade;
+
+            if(Quantidade > 3)
+            {
+                return valorTotal * 0.20;
+            }
+
+            return 0.0;
+        }
+        public double CalcularValorFinal()
+        {
+            double valorBruto = PrecoServ * Quantidade;
+            double desconto = CalcularDesconto();
+            return valorBruto = desconto;
+        }
+        public double CalcularParcela()
+        {
+            return CalcularValorFinal() / 3;
+        }
+        public bool ValidarBrinde()
+        {
+            return (IdadeAnimal >= 18 && Quantidade >= 5 || CalcularValorFinal() > 100);
+        }
+
+    }
+}
